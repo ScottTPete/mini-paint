a4916b8e97be33d23983355b78bdaf4e47833cba
Scott Peterson <stpeterson28@yahoo.com>
2016-02-11 14:42:57 -0700

    finished paint project, added drag and a pointer over reset

--- app.js
+++ app.js
@@ -1,40 +1,124 @@
 $(document).ready(function(){
 
-	var color = "white";
-		//selector //listener //callback
-	$('.box').on("click", function(){
-		$(this).addClass(color); // function body
-	})
+  var box = $('.box');
+	var color = 'white';
+	var colors = 'white green red blue yellow'
 
-	$(".box").on('dblclick', function(){
-		$(this).removeClass(color);
+	box.on('click', function(){
+		$(this).addClass(color);
 	})
-
+	box.on('dblclick', function(){
+		$(this).removeClass(colors);
+	})
 	$('#reset').on('click', function(){
-		$('.box').removeClass('red blue green yellow white');
+		box.removeClass(color);
 	})
+	var isPressed = false;
+	$('.box').on('mousedown', function(){
+		isPressed = true;
+	});
 
+	$('.box').on('mouseup', function(){
+		isPressed = false;
+	});
+
+	$('.box').on('mouseenter', function(){
+		if(isPressed){
+			$(this).addClass(color);
+		}
+	});
 	$('#red').on('click', function(){
 		color = 'red';
 	})
-
 	$('#blue').on('click', function(){
 		color = 'blue';
 	})
-
 	$('#green').on('click', function(){
 		color = 'green';
 	})
-
 	$('#yellow').on('click', function(){
 		color = 'yellow';
 	})
-
 	$('#white').on('click', function(){
 		color = 'white';
 	})
-
-
-
-
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//
+// 	var color = "white";
+// 		//selector //listener //callback
+// 	$('.box').on("click", function(){
+// 		$(this).addClass(color); // function body
+// 	})
+//
+// 	$(".box").on('dblclick', function(){
+// 		$(this).removeClass(color);
+// 	})
+//
+// 	$('#reset').on('click', function(){
+// 		$('.box').removeClass('red blue green yellow white');
+// 	})
+//
+// 	$('#red').on('click', function(){
+// 		color = 'red';
+// 	})
+//
+// 	$('#blue').on('click', function(){
+// 		color = 'blue';
+// 	})
+//
+// 	$('#green').on('click', function(){
+// 		color = 'green';
+// 	})
+//
+// 	$('#yellow').on('click', function(){
+// 		color = 'yellow';
+// 	})
+//
+// 	$('#white').on('click', function(){
+// 		color = 'white';
+// 	})
+//
+//
+//
+//
+// })
